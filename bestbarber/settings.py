a63b76b0304b742920eeaa844e07bdcd61a0431d
Ezad Zamily <2680737E@student.gla.ac.uk>
2023-03-08 15:43:32 +0000

    Change BASE_DIR in settings.py

--- bestbarber/settings.py
+++ bestbarber/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "bestbarber.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
