75bf8fcd8f9124f4c513b92dc5c9ca995574657d
Ezad Zamily <2680737E@student.gla.ac.uk>
2023-03-08 15:58:01 +0000

    Added title block to base.html

--- bestbarber/urls.py
+++ bestbarber/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('barbers.urls')),
-    path('accounts/', include('registration.backends.simple.urls')),
+    path('accounts/', include('registration.backends.default.urls')),
 ]
