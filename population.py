854d2e1ccd329f46876a4b9989ae34110ee983ec
Jiusi Zhao <2541919Z@student.gla.ac.uk>
2023-03-21 15:00:13 +0000

    update population script

--- population.py
+++ population.py
@@ -9,10 +9,10 @@ django.setup()
 from barbers.models import BarberShop, User
 
 
-
 def populate():
     barbers_barbershop = [
         {
+            'id': 1,
             'name': 'shop1',
             'location': 'location1',
             'description': 'barber1_description',
@@ -21,6 +21,7 @@ def populate():
             'style': 'style1',
             'price': 10},
         {
+            'id': 2,
             'name': 'shop2',
             'location': 'location2',
             'description': 'barber2_description',
@@ -31,16 +32,17 @@ def populate():
 
     ]
 
-
     barbers_barbershop2 = [
-        {'name': 'shop3',
+        {'id':3,
+        'name': 'shop3',
          'location': 'location3',
          'description': 'barber3_description',
          'service': 'barber3_service',
          'type': 'type3',
          'style': 'style3',
          'price': 30},
-        {'name': 'shop4',
+        {'id':4,
+        'name': 'shop4',
          'location': 'location4',
          'description': 'barber4_description',
          'service': 'barber4_service',
@@ -48,22 +50,19 @@ def populate():
          'style': 'style4',
          'price': 40},
 
-
     ]
 
     barbers_managerprofile = {'manager1': {'password': '11111', 'shop': barbers_barbershop},
                               'manager2': {'password': '11111', 'shop': barbers_barbershop2}}
 
-
     def add_user(name, password):
-
         user = User.objects.get_or_create(username=name)[0]
         user.set_password(password)
         user.save()
         return user
 
-    def add_shop(user, name, location, description, service, type, style, price):
-        c = BarberShop.objects.get_or_create(manage_by=user)[0]
+    def add_shop(user,id, name, location, description, service, type, style, price):
+        c = BarberShop.objects.get(manage_by=user,id = id)
         c.name = name
         c.location = location
         c.description = description
@@ -77,7 +76,9 @@ def populate():
     for manager, manager_data in barbers_managerprofile.items():
         user = add_user(manager, manager_data["password"])
         for p in manager_data['shop']:
+            print(p)
             add_shop(user,
+                     p['id'],
                      p['name'],
                      p['location'],
                      p['description'],
