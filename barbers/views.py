9376c5813971f503da6f6abc05af113096ab72af
Idealcan Ayavefe <2785541a@student.gla.ac.uk>
2023-03-23 15:10:28 +0000

    added empty star

--- barbers/views.py
+++ barbers/views.py
@@ -122,12 +122,14 @@ def barbers(request):
     context_dict = {}
     barbers_list = Barbershop.objects.order_by('-name')
     for shop in barbers_list:
-        if shop.user_attr is not None:
-            shop.user_attr = shop.user_attr.split(",")  # convert user attributes string into list
+        if len(shop.user_attr) > 1:
+            shop.user_attr = shop.user_attr.split(",")  # convert user attributes string into list]
+        else:
+            shop.user_attr = ['?','?','?']
+        
 
-    context_dict['barberShops'] = barbers_list
+    context_dict['barbershops'] = barbers_list
 
-    visitor_cookie_handler(request)
     response = render(request, 'barbers/barbers.html', context=context_dict)
 
     return response
@@ -253,37 +255,6 @@ def add_barber(request):
                   'barbers/add_barbers.html',
                   context={'barber_form': barber_form,
                            'registered': registered})
-
-
-# A helper method
-def get_server_side_cookie(request, cookie, default_val=None):
-    val = request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-
-# Updated the function definition
-def visitor_cookie_handler(request):
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request,
-                                               'last_visit',
-                                               str(datetime.now()))
-
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
-                                        '%Y-%m-%d %H:%M:%S')
-
-    # If it's been more than a day since the last visit...
-    if (datetime.now() - last_visit_time).days > 0:
-        visits = visits + 1
-        # Update the last visit cookie now that we have updated the count
-        request.session['last_visit'] = str(datetime.now())
-    else:
-        # Set the last visit cookie
-        request.session['last_visit'] = last_visit_cookie
-
-    # Update/set the visits cookie
-    request.session['visits'] = visits
 
 class ProfileView(View):
     def get_user_details(self, username):
