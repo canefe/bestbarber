e9b02c8908fdd5e8ad2d3cd60ecb9958e9ce1e1a
Idealcan Ayavefe <2785541a@student.gla.ac.uk>
2023-03-24 14:51:42 +0000

    FIX CSS input form

--- static/js/forms_util.js
+++ static/js/forms_util.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
     $('.peer').on('input', function() {
+        console.log('input');
         var $span = $(this).siblings('.peer-span');
         if ($(this).val().length > 0) {
-            $span.addClass('-top-12');
-            $span.addClass('text-xs');
+            $span.addClass('!-top-12');
+            $span.addClass('!text-xs');
         }
     });
     //focus out
     $('.peer').on('focusout', function() {
         var $span = $(this).siblings('.peer-span');
         if ($(this).val().length == 0) {
-            $span.removeClass('-top-12');
-            $span.removeClass('text-xs');
+
+            $span.removeClass('!-top-12');
+            $span.removeClass('!text-xs');
+        } else if ($(this).val().length > 0) {
+            $span.addClass('!-top-12');
+            $span.addClass('!text-xs');
         }
     });
 });
